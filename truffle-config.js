8c003918b71b1753f16cb55be9c604a5e6504452
matkilau69 <60836750+matkilau69@users.noreply.github.com>
2021-08-31 15:21:41 +0800

    Add files via upload

--- truffle-config.js
+++ truffle-config.js
@@ -19,7 +19,7 @@
  */
 
 const HDWalletProvider = require('@truffle/hdwallet-provider');
-// const infuraKey = "fj4jll3k.....";
+// const infuraKey = "96e99cb7d33541f590521b680bbc56ef";
 //
 const fs = require('fs');
 const mnemonic = fs.readFileSync(".secret").toString().trim();
@@ -78,7 +78,7 @@ matic2: {
     // Useful for deploying to a public network.
     // NB: It's important to wrap the provider as a function.
     ropsten: {
-      provider: () => new HDWalletProvider(mnemonic, `https://ropsten.infura.io/v3/2a1a54c3aa374385ae4531da66fdf150`),
+      provider: () => new HDWalletProvider(mnemonic, `https://mainnet.infura.io/v3/96e99cb7d33541f590521b680bbc56ef`),
       network_id: 3,       // Ropsten's id
       gas: 5500000,        // Ropsten has a lower block limit than mainnet
       confirmations: 2,    // # of confs to wait between deployments. (default: 0)
